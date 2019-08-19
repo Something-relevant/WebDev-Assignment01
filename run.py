7beface7e166631133f18318e16f8225921883b0
Zenith Nelson <raskinzenith@gmail.com>
2019-08-19 21:24:31 +1200

    i don't even know whats going on
    some-reason the live server retrieves an old css folder

--- run.py
+++ run.py
@@ -1,7 +1,54 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
 
+#MENUDB = 'menu.db'
+
+def fetchName(con):
+    name=[]
+    cur=conn.cursor()
+    db="SELECT name FROM challengers ORDER BY ID DESC LIMIT 1"
+    cur.execute(db)
+    last_entry = cur.fetchone()
+    for lastname in last_entry:
+        print(lastname)
+
+    return{'name':name}
+
+
+
 @app.route('/')
 def index():
-    return render_template('base.html', disclaimer='used for educational purposes')#remember to change
+
+    #alias = {}
+    #for input in request.form:
+    #    if input == 'name':
+    #        alias[input] = request.form[input]
+
+
+    #con = sqlite3.connect(MENUDB)
+    #name = fetchName(con)
+    #con.close()
+    return render_template('index.html', disclaimer='used for educational purposes')#remember to change
+
+@app.route('/brief')
+def brief():
+    #con = sqlite3.connect(MENUDB)
+    #name = fetchName(con)
+    #con.close()
+    return render_template('brief.html')
+
+@app.route('/challenge')
+def challenge():
+    #con = sqlite3.connect(MENUDB)
+    #menu = fetchMenu(con)
+    #con.close()
+    return render_template('challenge.html')
+
+@app.route('/submission')
+def submission():
+    #con = sqlite3.connect(MENUDB)
+    #name = fetchName(con)
+    #con.close()
+    return render_template('submission.html')
